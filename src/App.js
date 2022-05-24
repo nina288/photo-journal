b217a8f77325d696c72194c0d9e1d768c19100a8
nina288 <ninachestnaja@gmail.com>
2022-05-24 20:23:06 +0300

    Photo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import BannerImage from './components/BannerImage';
+import AlbumMaker from './components/AlbumMaker';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <BannerImage />
+        <div>
+        <h1> Давайте влюбляться</h1>
+        <h3>24.05.2022 @Чекалина Н.</h3>
+        <h7>
+        <quote>  Давайте влюбляться, как в юности – ярко.
+          Давайте дарить теплоту и подарки,
+          За руки давайте возьмем мы друг - друга
+          И выйдем из скуки порочного круга.
+          Давайте мы память от пыли встряхнем
+          И день, что грядет, мы с улыбки начнем.
+          Любимым подарим свиданья - как раньше.
+          Забудем о ссорах, забудем о фальши.
+          Давайте изменим своё отношенье
+          К тому, что вокруг, и изменим решенье.
+          Давайте макнем этот мир в чудо - краски
+          И душу избавим от серости маски.</quote></h7>
+        <AlbumMaker/>
+        </div>
+      
       </header>
     </div>
   );
