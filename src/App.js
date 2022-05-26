6f69dfbb9e2300b8be8ae27372602f317ccbaf65
nina288 <ninachestnaja@gmail.com>
2022-05-26 17:48:34 +0300

    Adding to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import BannerImage from './components/BannerImage';
 import AlbumMaker from './components/AlbumMaker';
+import UserManager from './components/UserManager';
 import './App.css';
+import ContentArea from './components/ContentArea';
 
 function App() {
   return (
@@ -9,22 +11,13 @@ function App() {
       <header className="App-header">
        <BannerImage />
         <div>
-        <h1> Давайте влюбляться</h1>
-        <h3>24.05.2022 @Чекалина Н.</h3>
-        <h7>
-        <quote>  Давайте влюбляться, как в юности – ярко.
-          Давайте дарить теплоту и подарки,
-          За руки давайте возьмем мы друг - друга
-          И выйдем из скуки порочного круга.
-          Давайте мы память от пыли встряхнем
-          И день, что грядет, мы с улыбки начнем.
-          Любимым подарим свиданья - как раньше.
-          Забудем о ссорах, забудем о фальши.
-          Давайте изменим своё отношенье
-          К тому, что вокруг, и изменим решенье.
-          Давайте макнем этот мир в чудо - краски
-          И душу избавим от серости маски.</quote></h7>
-        <AlbumMaker/>
+          <ContentArea />
+        <h1> Notice your senses...</h1>
+        <h3>26.05.2022 @Chekalina</h3>
+
+        
+        
+       
         </div>
       
       </header>
